a7b6b1e4190a1f1c9f57acf3fdd63d5f65dd4133
acardo02 <00037820@uca.edu.sv>
2023-05-31 15:15:47 -0600

    Labo 10

---
+++ Laboratorio10 - 00037820/app/src/main/java/com/example/laboratorio05/data/dao/ActorDao.kt
@@ -0,0 +1,18 @@
+package com.example.laboratorio05.data.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Transaction
+import com.example.laboratorio05.data.model.ActorModel
+import com.example.laboratorio05.data.model.MovieModel
+
+@Dao
+interface ActorDao {
+    @Query("SELECT * FROM actor_table")
+    suspend fun getAllActor(): List<ActorModel>
+
+    @Transaction
+    @Insert
+    suspend fun insertActors(actor: ActorModel)
+}
